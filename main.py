3a676b176b297857dc2672ede9ef7e7eccdc0e3c
Visakha Lisa Jiamsirioungkul <visakhalisajiam@gmail.com>
2017-06-28 17:41:43 -0700

    not finished

--- main.py
+++ main.py
@@ -18,22 +18,24 @@ class Blog(db.Model):
         self.title = title
         self.content = content
 
-
+# TODO: #landing page redirected to blog
 @app.route('/', methods=['POST', 'GET'])
 def index():
     return redirect('/blog')
 
+# TODO:redirected from / showing all blogs
 @app.route('/blog', methods=['POST', 'GET'])
 def all_blog():
     blogs = Blog.query.all()
-    return render_template('blogs.html', title='Your Blog', blogs = blogs)
+    return render_template('blog.html', title='Your Blog', blogs = blogs)
 
+# TODO: query all blogs and return/gets the selected blog
 def selected_blog():
     blog_id = request.args.get('id')
     blog_post = Blog.query.filter_by(blog_id).first()
     return render_template('selected_blog.html', title='Selected Blog' , blogs = blogs)
 
-
+# TODO: new post and check for errors
 @app.route('/newpost', methods=['POST','GET'])
 def new_blog():
     #currently no errors
@@ -44,6 +46,7 @@ def new_blog():
         blog_title = request.form['title']
         blog_content = request.form['content']
         new_post = Blog(title=blog_title,content=blog_content)
+
     # TODO: no title error send to newpost
     elif blog_title == "":
         title_error = 'Eh...Try Again'
@@ -60,8 +63,6 @@ def new_blog():
         db.session.commit()
         new_blog = str(blog_title, blog_content)
     return render_template('newpost.html', title='New Blog')
-
-
 
 if __name__ == '__main__':
     app.run()
